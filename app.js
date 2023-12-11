451e0d9b30a6c139cd8bdfc664c98ebae119eaa0
laragele <142636055+laragele@users.noreply.github.com>
2023-12-11 13:49:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// npm init -y 
+//npm install express
+//npm install joi
+
+const express = require(`express`);
+const app=express();
+const port= 3000
+
+const librosRouters= require(`./routes/libros`);
+const errorHandler=require(`./middleware/errorHandler`);
+
+app.use(express.json());
+
+app.use(`/libros`, librosRouters);
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Servidor iniciado correctamente y en funcionamiento en el puerto: ${port}`);
+})
---
+++ data.js
@@ -0,0 +1,7 @@
+const libros= [
+    {id:`1`, titulo:`Harry Potter: y la piedra filosofal`, autor:`J. K. Rowling`},
+    {id:`2`, titulo:`Los Juegos del Hambre`, autor:`Suzanne Collins`},
+    {id:`3`, titulo:`1984`, autor:`George Orwell`}
+]
+
+module.exports=libros;
